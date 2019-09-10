afa67ebb6cb3f78435fc2bdbbef89608093b8c93
tianxing <jackcaotianxing@163.com>
2019-09-10 22:12:21 +0800

    完成项目前期基本配置

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import routes from './config/routes'
+
+class App extends React.Component {
+
+    render() {
+        return <Router>
+            {
+                routes.map((route,index)=>{
+                    return <Route{...route} key={index}/>
+                })
+            }
+
+        </Router>
+    }
+}
+
+export default App;
